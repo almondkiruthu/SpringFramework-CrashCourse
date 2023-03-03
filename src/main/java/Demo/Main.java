b9efd7c013dc906e50cbdfab02b44f62a7b30ab6
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-03-03 20:17:21 +0300

    I created the Java configuration file

--- src/main/java/Demo/Main.java
+++ src/main/java/Demo/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 
-        Doctor staff = context.getBean(Doctor.class);
+        Staff staff = context.getBean(Doctor.class);
         staff.assist();
     }
 }
