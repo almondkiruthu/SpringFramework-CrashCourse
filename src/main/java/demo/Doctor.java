f7c0b39bc61af2642b1d3ffd871ade11b32e0005
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-03-03 17:26:03 +0300

    Added the essentials files, created the Mian Class and the Doctor class to Start up on my project

---
+++ src/main/java/demo/Doctor.java
@@ -0,0 +1,10 @@
+package demo;
+
+
+import org.springframework.stereotype.Component;
+
+public class Doctor{
+    public void assist() {
+        System.out.println("Doctor is assisting");
+    }
+}
