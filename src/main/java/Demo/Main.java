718f0a9c8e1d1ab98020e9de4ecae8ba46e50976
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-03-03 20:01:35 +0300

    Modified the Main.Java file

--- src/main/java/Demo/Main.java
+++ src/main/java/Demo/Main.java
@@ -1,13 +1,12 @@
 package Demo;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 
         Doctor staff = context.getBean(Doctor.class);
         staff.assist();
